642c4d9eb278e748521d921b59921f2fa9f701ba
Vanessa Bell <vanessa.bell14@gmail.com>
2016-10-18 14:13:23 -0700

    prepping for command-line heroku deployment

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var cons = require('consolidate');
 
 require('./server/config/routes.js')(app);
 app.engine('html', cons.swig);
+app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'html');
 
 
 app.use(express.static(path.join(__dirname,'/views')));
-app.listen(8000,function() {
-	console.log('personal website on 8000');
+app.listen(app.get('port'),function() {
+	console.log('personal website on 5000');
 });
